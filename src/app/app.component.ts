eb954fce8ec08f82f86ecbc7a04decdbff2ad34a
209420 <209420@edu.p.lodz.pl>
2019-12-02 13:38:09 +0100

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reportingSystemWeb';
+
+  public logout() {
+    // this.oauthService.logOut()
+  }
+
+  public get logged() {
+    // return this.oauthService.hasValidAccessToken();
+    return true;
+  }
 }
