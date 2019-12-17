1128ce3e0029c72f4d23a90f3f81444b45b0a10a
209420 <209420@edu.p.lodz.pl>
2019-12-17 21:26:07 +0100

    change url to prod

--- src/app/service/global.ts
+++ src/app/service/global.ts
@@ -1,6 +1,7 @@
 import {HttpHeaders} from '@angular/common/http';
 
-export const apiURL = 'http://localhost:8080';
+// export const apiURL = 'http://localhost:8080';
+export const apiURL = 'https://reportingsystembackend.herokuapp.com';
 export const header = new HttpHeaders({
   Authorization: localStorage.getItem('token')
 });
